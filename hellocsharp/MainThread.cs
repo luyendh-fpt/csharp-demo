1a30c55e995beab1f9ac03ee438539463f9e7140
Hong Luyen <luyendh@fpt.edu.vn>
2018-04-04 09:39:28 +0700

    Tái cấu trúc project.

---
+++ hellocsharp/MainThread.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HelloCSharp
+{
+    public class MainThread
+    {
+        public MainThread()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            StudentModel model = new StudentModel();
+            Student student = new Student("Lộc", "D00124");
+            model.Insert(student);
+        }
+    }
+}
---
+++ hellocsharp/StudentModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace HelloCSharp
+{
+    public class StudentModel
+    {
+        private DbConnection dbConnection = new DbConnection();
+
+        public StudentModel()
+        {
+        }
+
+        public bool Insert(Student student)
+        {
+            string query = "INSERT INTO students (name, rollNumber) VALUES ('" + student.Name + "', '" + student.RollNumber + "')";
+            //open connection
+            if (dbConnection.OpenConnection() == true)
+            {
+                try
+                {
+                    //create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection());
+                    //Execute command
+                    cmd.ExecuteNonQuery();
+                    //close connection
+                    dbConnection.CloseConnection();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error " + ex.Message);
+                }
+            }
+            return false;
+        }
+    }
+}
